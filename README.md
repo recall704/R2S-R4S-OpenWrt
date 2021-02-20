f97137bb36c474521872c3fc306b0085cee3b5c1
msylgj <msylgj@vip.qq.com>
2021-02-20 11:53:44 +0800

    release

---
+++ README.md
@@ -0,0 +1,48 @@
+# NanoPi-R2S/R4S-OpenWrt
+ 使用 Github Actions 在线编译 NanoPi-R2S/R4S 固件
+
+![R2S Openwrt](https://github.com/msylgj/R2S-R4S-OpenWrt/workflows/R2S%20Openwrt%20From%20QiuSimons/badge.svg)
+![R4S Openwrt](https://github.com/msylgj/R2S-R4S-OpenWrt/workflows/R4S%20Openwrt%20From%20QiuSimons/badge.svg)
+## 说明
+* Fork自QiuSimons大神的纯原生OP版本,个人根据**完全私人**口味进行了一定修改,建议去源库了解更多
+    - [QiuSimons/R2S-R4S-X86-OpenWrt](https://github.com/QiuSimons/R2S-R4S-X86-OpenWrt)
+* ipv4: 192.168.2.1
+* username: root
+* password: 空
+* 原汁原味非杂交! 感谢R2S Club及R4S Club/天灵/GC/QC等诸多大佬的努力!
+* 添加Flow Offload/SFE(只能二选一,默认开启SFE)+Full Cone Nat+BBR
+* 支持scp和sftp
+* 无usb-wifi支持.wan&lan交换(r2s)
+* 原生OP内置升级可用,固件重置可用
+* 支持SSD1306驱动的12864(0.96英寸)和12832(0.91英寸)OLED屏幕(r2s)
+* OC-1.6(r2s)/2.2-1.8(r4s)
+
+## 插件清单
+- app:argon-config
+- app:arpbind
+- app:autoreboot
+- app:cpufreq
+- app:cpulimit
+- app:frpc(r2s)
+- app:frps(r4s)
+- app:oled(r2s)
+- app:openclash
+- app:ramfree
+- app:serverchan
+- app:tencentddns
+- app:unblockneteasemusic
+- app:upnp
+- app:vlmcsd
+- app:wol-plus
+- theme:argon
+- theme:bootstrap
+
+## 升级方法
+* 原生OP内置升级,可选保留配置
+* reset按钮可用(需要fork后自行修改yml编译squashfs格式固件)
+* 刷写或升级后遇到任何问题，可以尝试ssh进路由器，输入fuck，回车后等待重启，或可解决(需要fork后自行修改yml编译squashfs格式固件,来自QiuSimons)
+
+## 特别感谢（排名不分先后）
+* [QiuSimons/R2S-R4S-X86-OpenWrt](https://github.com/project-openwrt/R2S-OpenWrt)
+* [coolsnowwolf/lede](https://github.com/coolsnowwolf/lede)
+* [immortalwrt/immortalwrt](https://github.com/immortalwrt/immortalwrt)
---
+++ SCRIPTS/prepare_package.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+clear
+
+#凑合解决方案
+#wget -qO - https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/3875.patch | patch -p1
+
+#使用O2级别的优化
+sed -i 's/Os/O2/g' include/target.mk
+#更新feed
+./scripts/feeds update -a
+./scripts/feeds install -a -f
+#irqbalance
+sed -i 's/0/1/g' feeds/packages/utils/irqbalance/files/irqbalance.config
+#remove annoying snapshot tag
+sed -i 's,-SNAPSHOT,,g' include/version.mk
+sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
+
+##必要的patch
+wget -P target/linux/generic/pending-5.4 https://github.com/immortalwrt/immortalwrt/raw/master/target/linux/generic/hack-5.4/312-arm64-cpuinfo-Add-model-name-in-proc-cpuinfo-for-64bit-ta.patch
+#patch jsonc
+wget -q https://github.com/QiuSimons/R2S-R4S-X86-OpenWrt/raw/master/PATCH/new/package/use_json_object_new_int64.patch
+patch -p1 < ./use_json_object_new_int64.patch
+#patch dnsmasq
+wget -q https://github.com/QiuSimons/R2S-R4S-X86-OpenWrt/raw/master/PATCH/new/package/dnsmasq-add-filter-aaaa-option.patch
+wget -q https://github.com/QiuSimons/R2S-R4S-X86-OpenWrt/raw/master/PATCH/new/package/luci-add-filter-aaaa-option.patch
+wget -P package/network/services/dnsmasq/patches/ https://github.com/QiuSimons/R2S-R4S-X86-OpenWrt/raw/master/PATCH/new/package/900-add-filter-aaaa-option.patch
+patch -p1 < ./dnsmasq-add-filter-aaaa-option.patch
+patch -p1 < ./luci-add-filter-aaaa-option.patch
+rm -rf ./package/base-files/files/etc/init.d/boot
+wget -P package/base-files/files/etc/init.d https://github.com/immortalwrt/immortalwrt/raw/openwrt-18.06-k5.4/package/base-files/files/etc/init.d/boot
+#（从这行开始接下来4个操作全是和fullcone相关的，不需要可以一并注释掉，但极不建议
+# Patch Kernel 以解决fullcone冲突
+pushd target/linux/generic/hack-5.4
+wget https://github.com/coolsnowwolf/lede/raw/master/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
+popd
+#Patch FireWall 以增添fullcone功能
+mkdir package/network/config/firewall/patches
+wget -P package/network/config/firewall/patches/ https://github.com/immortalwrt/immortalwrt/raw/master/package/network/config/firewall/patches/fullconenat.patch
+# Patch LuCI 以增添fullcone开关
+wget -q https://github.com/QiuSimons/R2S-R4S-X86-OpenWrt/raw/master/PATCH/new/package/luci-app-firewall_add_fullcone.patch
+patch -p1 < ./luci-app-firewall_add_fullcone.patch
+#FullCone 相关组件
+cp -rf ../openwrt-lienol/package/network/fullconenat ./package/network/fullconenat
+#（从这行开始接下来3个操作全是和SFE相关的，不需要可以一并注释掉，但极不建议
+# Patch Kernel 以支援SFE
+pushd target/linux/generic/hack-5.4
+wget https://github.com/coolsnowwolf/lede/raw/master/target/linux/generic/hack-5.4/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
+popd
+# Patch LuCI 以增添SFE开关
+wget -q https://github.com/QiuSimons/R2S-R4S-X86-OpenWrt/raw/master/PATCH/new/package/luci-app-firewall_add_sfe_switch.patch
+patch -p1 < ./luci-app-firewall_add_sfe_switch.patch
+# SFE 相关组件
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shortcut-fe package/lean/shortcut-fe
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/fast-classifier package/lean/fast-classifier
+wget -P package/base-files/files/etc/init.d/ https://github.com/QiuSimons/R2S-R4S-X86-OpenWrt/raw/master/PATCH/duplicate/shortcut-fe
+
+##获取额外package
+#（不用注释这里的任何东西，这不会对提升action的执行速度起到多大的帮助
+#（不需要的包直接修改seed就好
+#upx
+sed -i '/patchelf pkgconf/i\tools-y += ucl upx' ./tools/Makefile
+sed -i '\/autoconf\/compile :=/i\$(curdir)/upx/compile := $(curdir)/ucl/compile' ./tools/Makefile
+svn co https://github.com/immortalwrt/immortalwrt/branches/master/tools/upx tools/upx
+svn co https://github.com/immortalwrt/immortalwrt/branches/master/tools/ucl tools/ucl
+#R8168
+svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/ctcgfw/r8168 package/new/r8168
+patch -p1 < ../SCRIPTS/led.patch
+sed -i '/r8169/d' ./target/linux/rockchip/image/armv8.mk
+#更换cryptodev-linux
+rm -rf ./package/kernel/cryptodev-linux
+svn co https://github.com/openwrt/openwrt/trunk/package/kernel/cryptodev-linux package/kernel/cryptodev-linux
+#更换Node版本
+rm -rf ./feeds/packages/lang/node
+svn co https://github.com/nxhack/openwrt-node-packages/trunk/node feeds/packages/lang/node
+rm -rf ./feeds/packages/lang/node-arduino-firmata
+svn co https://github.com/nxhack/openwrt-node-packages/trunk/node-arduino-firmata feeds/packages/lang/node-arduino-firmata
+rm -rf ./feeds/packages/lang/node-cylon
+svn co https://github.com/nxhack/openwrt-node-packages/trunk/node-cylon feeds/packages/lang/node-cylon
+rm -rf ./feeds/packages/lang/node-hid
+svn co https://github.com/nxhack/openwrt-node-packages/trunk/node-hid feeds/packages/lang/node-hid
+rm -rf ./feeds/packages/lang/node-homebridge
+svn co https://github.com/nxhack/openwrt-node-packages/trunk/node-homebridge feeds/packages/lang/node-homebridge
+rm -rf ./feeds/packages/lang/node-serialport
+svn co https://github.com/nxhack/openwrt-node-packages/trunk/node-serialport feeds/packages/lang/node-serialport
+rm -rf ./feeds/packages/lang/node-serialport-bindings
+svn co https://github.com/nxhack/openwrt-node-packages/trunk/node-serialport-bindings feeds/packages/lang/node-serialport-bindings
+#luci-app-freq
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/lean/luci-app-cpufreq package/lean/luci-app-cpufreq
+#arpbind
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/lean/luci-app-arpbind package/lean/luci-app-arpbind
+#AutoCore
+svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/lean/autocore package/lean/autocore
+svn co https://github.com/immortalwrt/packages/trunk/utils/coremark feeds/packages/utils/coremark
+ln -sf ../../../feeds/packages/utils/coremark ./package/feeds/packages/coremark
+sed -i 's,default n,default y,g' feeds/packages/utils/coremark/Makefile
+#oled
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/others/luci-app-oled package/new/luci-app-oled
+#网易云解锁
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/others/luci-app-unblockneteasemusic package/new/UnblockNeteaseMusic
+#定时重启
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/lean/luci-app-autoreboot package/lean/luci-app-autoreboot
+#argon主题
+git clone -b master --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/new/luci-theme-argon
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/others/luci-app-argon-config package/new/luci-app-argon-config
+#luci-app-cpulimit
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/others/luci-app-cpulimit package/lean/luci-app-cpulimit
+svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/ntlf9t/cpulimit package/lean/cpulimit
+#清理内存
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/lean/luci-app-ramfree package/lean/luci-app-ramfree
+#OpenClash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/new/luci-app-openclash
+#SeverChan
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/others/luci-app-serverchan package/new/luci-app-serverchan
+sed -i 's/--interface ${ipv._interface} //g' package/new/luci-app-serverchan/root/usr/bin/serverchan/serverchan
+#UPNP（回滚以解决某些沙雕设备的沙雕问题
+rm -rf ./feeds/packages/net/miniupnpd
+svn co https://github.com/coolsnowwolf/packages/trunk/net/miniupnpd feeds/packages/net/miniupnpd
+#KMS
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/lean/luci-app-vlmcsd package/lean/luci-app-vlmcsd
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/vlmcsd package/lean/vlmcsd
+#frp
+rm -rf ./feeds/luci/applications/luci-app-frps
+rm -rf ./feeds/luci/applications/luci-app-frpc
+rm -rf ./feeds/packages/net/frp
+rm -f ./package/feeds/packages/frp
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/lean/luci-app-frps package/lean/luci-app-frps
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/lean/luci-app-frpc package/lean/luci-app-frpc
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/frp package/lean/frp
+#腾讯DDNS
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/others/luci-app-tencentddns package/new/luci-app-tencentddns
+#WOL
+svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/others/luci-app-services-wolplus package/new/luci-app-services-wolplus
+
+##最后的收尾工作
+#Lets Fuck
+#mkdir package/base-files/files/usr/bin
+#cp -f ../SCRIPTS/fuck package/base-files/files/usr/bin/fuck
+#最大连接
+sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+#custom config
+sed -i "s/'%D %V %C'/'Built by OPoA($(date +%Y.%m.%d))@%D %V %C'/g" package/base-files/files/etc/openwrt_release
+sed -i "/%D/a\ Built by OPoA($(date +%Y.%m.%d))" package/base-files/files/etc/banner
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+#修改启动等待（可能无效）
+sed -i 's/default "5"/default "0"/g' config/Config-images.in
+#生成默认配置及缓存
+rm -rf .config
+exit 0
